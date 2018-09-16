378b09273c179ed12a312d022f173fca330f954d
Clarence Chan <cchan@live.ca>
2018-09-16 14:51:10 -0400

    Split the courses into seperate elements.

--- scripts/parser.js
+++ scripts/parser.js
@@ -2,9 +2,13 @@
 window.onload = () => { document.querySelector('#parse_button').onclick = handleParseClick; }
 
 function handleParseClick() {
+    // This parses the raw text from the print friendly seneca schedule given on the student center.
+    // Currently relies on the order, though with more regex, it'll be slower but more flexible and 
+    // work out of order.
+
     console.log('parsecalled')
 
-    // get and split raw text into lines
+    // Get and split raw text into lines
     const calendar = document.querySelector('#calendarRaw');
     const rawLines = calendar.innerHTML.split('\n');
 
@@ -17,20 +21,23 @@ function handleParseClick() {
     }
     console.log(tempArr);
 
-    // TODO: split into courses
+    // Split into registered courses
     let courses = [];
     for (let i = 0; i < tempArr.length; i++) {
-        if (tempArr[i][1] == "Enrolled") {
-
-            courses.push(
-                rawLines.slice(
-                    tempArr[i][1] - 2, tempArr[i+1][1] - 3
-                )
-            );
+        if (tempArr[i][0] == "Enrolled") {
+            if (i + 1 < tempArr.length)
+                courses.push(rawLines.slice(tempArr[i][1] - 2, tempArr[i + 1][1] - 2));
+            else
+                courses.push(rawLines.slice(tempArr[i][1] - 2));
         }
     }
-    //TODO: Split into classes
+    console.log(courses)
 
+    //TODO: Split into classes
+    let classes = [];
+
+
+    
     //TODO: Send to google calendar
 }
 
